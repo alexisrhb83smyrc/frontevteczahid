9b381e98dc189af757bdb5a287201a207b766568
ZahidYRamosV <191341@ids.upchiapas.edu.mx>
2023-11-09 11:48:56 -0600

    todo terminado y funcional

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter, Label } from 'reactstrap';
 
+import imagenes from './assets/imagenes.js';
 // calcula la edad intento 1
 // const fechann = document.getElementById("fechann");
 // const edad = document.getElementById("edad");
@@ -34,12 +35,12 @@ import { Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, Mod
 
 // "Base de datos" osea el array pe
 const data = [
-  { id: 1, personaje: "Naruto", fechann: "12-Mayo-1999", direccion: "Konoha", edad: "24", anime: "Naruto", foto: "base64" },
-  { id: 2, personaje: "Seto Kaiba", fechann: "25-octubre-1998", direccion: "Sierra Tarahumara", edad: "25", anime: "YUGIOH", foto: "base64" },
-  { id: 3, personaje: "Ichigo", fechann: "15-Julio-1988", direccion: "Pueblo Karakura", edad: "35", anime: "Bleach", foto: "base64" },
-  { id: 4, personaje: "Koro Sense", fechann: "12-Marzo-1993", direccion: "Kunugigaoka", edad: "29", anime: "Assassination Classroom", foto: "base64" },
-  { id: 5, personaje: "Mikasa", fechann: "10-febrero-1999", direccion: "Muralla Maria", edad: "24", anime: "Attack On Titan", foto: "base64" },
-  { id: 6, personaje: "Lawliet", fechann: "31-octubre-1979", direccion: "Inglaterra", edad: "24", anime: "Death Note", foto: "base64" },
+  { id: 1, personaje: "Naruto", fechann: "1999-05-12", direccion: "Konoha", edad: "24", anime: "Naruto", foto: imagenes.img1 },
+  { id: 2, personaje: "Seto Kaiba", fechann: "1998-10-25", direccion: "Sierra Tarahumara", edad: "25", anime: "YUGIOH", foto: imagenes.img2 },
+  { id: 3, personaje: "Ichigo", fechann: "1988-07-15", direccion: "Pueblo Karakura", edad: "35", anime: "Bleach", foto: imagenes.img3 },
+  { id: 4, personaje: "Koro Sense", fechann: "1993-03-12", direccion: "Kunugigaoka", edad: "29", anime: "Assassination Classroom", foto: imagenes.img4 },
+  { id: 5, personaje: "Mikasa", fechann: "1999-02-10", direccion: "Muralla Maria", edad: "24", anime: "Attack On Titan", foto: imagenes.img5 },
+  { id: 6, personaje: "Lawliet", fechann: "1979-10-31", direccion: "Inglaterra", edad: "24", anime: "Death Note", foto: imagenes.img6 },
 ];
 
 
@@ -58,14 +59,42 @@ class App extends React.Component {
     modalInsertar:false,
     modalEditar:false,
   };
-  handleChange=e=>{
-    this.setState({
-      form:{
-        ...this.state.form,
-        [e.target.name]: e.target.value,
-      }
-    });
-  }
+  handleChange = (e) => {
+    if (e.target.type === 'file') {
+      // Si es un campo de archivo, utiliza la función handleFileChange
+      this.handleFileChange(e);
+    } else {
+      // Para otros campos, actualiza el estado como antes
+      this.setState({
+        form: {
+          ...this.state.form,
+          [e.target.name]: e.target.value,
+        },
+      });
+    }
+  };
+  
+  handleFileChange = (e) => {
+    const file = e.target.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onloadend = () => {
+        // Cuando la lectura del archivo esté completa, actualiza el estado con la URL de la imagen
+        this.setState({
+          form: {
+            ...this.state.form,
+            foto: reader.result,
+          },
+        });
+      };
+  
+      // Leer el contenido del archivo como una URL de datos
+      reader.readAsDataURL(file);
+    }
+  };
+  
   // Modal Add
   mostrarModalInsertar=()=>{
     this.setState({modalInsertar:true});
@@ -81,31 +110,59 @@ class App extends React.Component {
     this.setState({modalEditar:false});
   }
 
-  insertar=()=>{
-    var valorNuevo={...this.state.form};
-    valorNuevo.id=this.state.data.length+1;
-    var lista=this.state.data;
-    lista.push(valorNuevo);
-    this.setState({data: lista, modalInsertar: false});
+  insertar = () => {
+    // Vamos a crear un nuevo elemento con las propiedades del formulario
+    var valorNuevo = {
+      id: this.state.data.length + 1,
+      personaje: this.state.form.personaje,
+      fechann: this.state.form.fechann,
+      direccion: this.state.form.direccion,
+      edad: this.state.edad,  // Asignar la edad calculada
+      anime: this.state.form.anime,
+      foto: this.state.form.foto,
+    };
+    console.log("Foto", this.state.form.foto);
+      // Agregar el nuevo elemento a la lista
+  var lista = this.state.data;
+  lista.push(valorNuevo);
 
-  }
-  editar=(dato)=>{
-    var contador=0;
-    var lista=this.state.data;
-    lista.map((registro)=>{
-      if(dato.id==registro.id){
-        lista[contador].personaje=dato.personaje;
-        lista[contador].fechann=dato.fechann;
-        lista[contador].direccion=dato.direccion;
-        lista[contador].edad=dato.edad;
-        lista[contador].anime=dato.anime;
-        lista[contador].foto=dato.foto;
+  // Actualizar el estado con la nueva lista y cerrar el modal
+  this.setState({ data: lista, modalInsertar: false });
+};
+  
+editar = (dato) => {
+  var contador = 0;
+  var lista = this.state.data;
+  lista.map((registro) => {
+    if (dato.id === registro.id) {
+      lista[contador].personaje = dato.personaje;
+      lista[contador].fechann = dato.fechann;
+      lista[contador].direccion = dato.direccion;
 
+      // Calcular la nueva edad en función de la nueva fecha de nacimiento
+      const fechaNacimiento = new Date(dato.fechann);
+      const fechaHoy = new Date();
+      let edad = fechaHoy.getFullYear() - fechaNacimiento.getFullYear();
+
+      // Comprueba si el cumpleaños ya ha ocurrido este año
+      if (
+        fechaNacimiento.getMonth() > fechaHoy.getMonth() ||
+        (fechaNacimiento.getMonth() === fechaHoy.getMonth() &&
+          fechaNacimiento.getDate() > fechaHoy.getDate())
+      ) {
+        edad--;
       }
-      contador++;
-    });
-    this.setState({data:lista, modalEditar: false});
-  }
+
+      lista[contador].edad = edad;
+
+      lista[contador].anime = dato.anime;
+      lista[contador].foto = dato.foto;
+    }
+    contador++;
+  });
+  this.setState({ data: lista, modalEditar: false });
+};
+
   eliminar=(dato)=>{
     var opcion=window.confirm("Estas seguro de borrarme?? 7u7");
     if(opcion){
@@ -120,12 +177,13 @@ class App extends React.Component {
       this.setState({data:lista});
     }
   }
-  //Calcula la edad intento 2
+  //Calcula la edad intento 2 y el bueno
   handleFechaDeNacimientoChange = (event) => {
+    console.log('entro');
     const fechaNacimiento = new Date(event.target.value);
     const fechaHoy = new Date();
     const edad = fechaHoy.getFullYear() - fechaNacimiento.getFullYear();
-  
+    
     // Comprueba si el cumpleaños ya ha ocurrido este año
     if (
       fechaNacimiento.getMonth() > fechaHoy.getMonth() ||
@@ -136,10 +194,14 @@ class App extends React.Component {
     }
   
     this.setState({
-      fechaDeNacimiento: event.target.value,
+      form: {
+        ...this.state.form,
+        fechann: event.target.value, // Actualizar fechann en lugar de fechaDeNacimiento 
+      },
       edad: edad,
     });
   };
+  
   render() {
     return (
       <>
@@ -167,7 +229,10 @@ class App extends React.Component {
                   <td>{elemento.direccion}</td>
                   <td>{elemento.edad}</td>
                   <td>{elemento.anime}</td>
-                  <td>{elemento.foto}</td>
+                  <td>
+                    {/* Mostrar la imagen */}
+                    {elemento.foto && <img src={elemento.foto} alt="Foto del personaje" style={{ maxWidth: '200px' }} />}
+                  </td>
                   <td>
                     <Button color='primary' onClick={()=>this.mostrarModalEditar(elemento)}>Editar</Button>{" "}    {/* aqui mandamos a llamar el modal para editar */}
                      <Button color='danger' onClick={()=>this.eliminar(elemento)}>Eliminar</Button>               {/* aqui mandamos a llamar la funcion eliminar */}
@@ -180,7 +245,9 @@ class App extends React.Component {
 
 {/* Add New Register */}
 
+
         <Modal isOpen={this.state.modalInsertar}>
+        
           <ModalHeader>
             <div>
               <h3>Add New Register</h3>
@@ -200,29 +267,29 @@ class App extends React.Component {
 
             <FormGroup>
               <label>Fecha De Nacimiento: </label>
-              <input className="form-control" name="fechann" type="date" onChange={this.handleFechaDeNacimientoChange=this.handleChange} value={this.state.fechaDeNacimiento} />
-              {/* <input type="date" value={this.state.fechaDeNacimiento} onChange={this.handleFechaDeNacimientoChange}/> */}
+              <input className="form-control" name="fechann" type="date" onChange={this.handleFechaDeNacimientoChange} value={this.state.fechaDeNacimiento} />              
             </FormGroup>
+            
             
             <FormGroup>
               <label>Direccion: </label>
               <input className="form-control" name="direccion" type="text" onChange={this.handleChange}/>
             </FormGroup>
-            <FormGroup>
-              <label>Edad: </label>
-              <input className="form-control" name="edad" type="text" onChange={this.handleChange}/>
-            </FormGroup> 
+           
             <FormGroup>
               <label>Anime: </label>
               <input className="form-control" name="anime" type="text"onChange={this.handleChange}/>
             </FormGroup>
             <FormGroup>
               <label>Foto: </label>
-              <input className="form-control" name="foto" type="img"onChange={this.handleChange} />
+              <input  className="form-control"   name="foto" type="file"onChange={this.handleFileChange}
+              />
             </FormGroup>
+
           </ModalBody>
 
           <ModalFooter>
+            
             <Button color='primary' onClick={()=>this.insertar()}>Insertar</Button> {/* Aqui confirmamos el Add */}
             <Button color='danger' onClick={()=>this.ocultarModalInsertar()}>Cancelar</Button> 
           </ModalFooter>
@@ -248,25 +315,35 @@ class App extends React.Component {
 
             <FormGroup>
               <label>Fecha De Nacimiento: </label>
-              <input className="form-control" name="fechann" type="date" onChange={this.handleChange} value={this.state.form.fechann}/>
+              <input className="form-control"name="fechann"type="date"onChange={(e) => {this.handleChange(e);this.handleFechaDeNacimientoChange(e);}}value={this.state.form.fechann}
+              />
             </FormGroup>
 
             <FormGroup>
               <label>Direccion: </label>
               <input className="form-control" name="direccion" type="text" onChange={this.handleChange} value={this.state.form.direccion}/>
             </FormGroup>
-            <FormGroup>
+            {/* <FormGroup>
               <label>Edad: </label>
               <input className="form-control" name="edad" type="text" onChange={this.handleChange} value={this.state.form.edad}/>
-            </FormGroup>
+            </FormGroup> */}
             <FormGroup>
               <label>Anime: </label>
               <input className="form-control" name="anime" type="text"onChange={this.handleChange} value={this.state.form.anime}/>
             </FormGroup>
             <FormGroup>
-              <label>Foto: </label>
-              <input className="form-control" name="foto" type="img"onChange={this.handleChange} value={this.state.form.foto}/>
+              <label>Foto: </label><br/>
+              <img
+                className="img-preview" style={{ maxWidth: '200px', width:'300px', display:'block', margin:'0 auto'}} // Puedes agregar estilos según sea necesario
+                src={this.state.form.foto}
+                alt="Preview"
+              />
+              <br/>
+              
+              <input className="form-control"type="file" onChange={this.handleFileChange}
+              />
             </FormGroup>
+
           </ModalBody>
 
           <ModalFooter>
---
+++ src/assets/imagenes.js
@@ -0,0 +1,19 @@
+import naruto from './img/naruto.png';
+import seto from './img/seto.png';
+import ichigo from './img/ichigo.png';
+import koro from './img/koro.png';
+import mikasa from './img/mikasa.png';
+import L from './img/L.png';
+
+
+
+
+export default{
+
+    "img1": naruto,
+    "img2": seto,
+    "img3": ichigo,
+    "img4": koro,
+    "img5": mikasa,
+    "img6": L,
+}
